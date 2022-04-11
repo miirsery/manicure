f7adc84c864661087511853850d3fbca968fbf1f
miirsery <miirsery.main@gmail.com>
2022-04-11 22:50:04 +0700

    updated slider

--- src/blocks/comments/comments.js
+++ src/blocks/comments/comments.js
@@ -8,12 +8,8 @@ document.addEventListener('DOMContentLoaded', () => {
     new Swiper(".comments__swiper", {
         slidesPerView: 1,
         modules: [Autoplay, Navigation, Pagination],
-        // speed: 500,
-        // loop: true,
-        // autoplay: {
-        //     delay: 2000,
-        //     disableOnInteraction: false,
-        // },
+        speed: 500,
+        loop: true,
         spaceBetween: 30,
         navigation: {
             nextEl: ".swiper-button-next",
@@ -23,4 +19,29 @@ document.addEventListener('DOMContentLoaded', () => {
             el: ".swiper-pagination",
         },
     })
+
+    const toggleButtons = document.querySelectorAll('.comments__button')
+    const hiddenComments = document.querySelectorAll('.comments__text-item--hidden')
+    const arrowsComments = document.querySelectorAll('.swiper-arrow')
+
+    const handleToggleComment = (item, className) => {
+        item.classList.toggle(className)
+    }
+
+    toggleButtons.forEach(button => {
+        button.addEventListener('click', () => {
+            hiddenComments.forEach(hiddenComment => handleToggleComment(hiddenComment, 'comments__text-item--active'))
+        })
+    })
+    arrowsComments.forEach(arrow => {
+        arrow.addEventListener('click', () => {
+            hiddenComments.forEach(comment => {
+                comment.classList.remove('comments__text-item--active')
+                // if (!item.classList.contains('active')) {
+                //     item.style.display = 'none'
+                // }
+            })
+        })
+    })
+
 })
