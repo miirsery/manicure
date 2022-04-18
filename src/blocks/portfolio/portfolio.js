61d4dcb36c81fe9fbaf12c9fc4f8fbd4da84792b
miirsery <miirsery.main@gmail.com>
2022-04-18 14:41:24 +0700

    stared adding mobile

--- src/blocks/portfolio/portfolio.js
+++ src/blocks/portfolio/portfolio.js
@@ -1,3 +1,9 @@
+import Swiper, { Autoplay, Navigation, Pagination } from "swiper"
+
+import "swiper/swiper.scss"
+import 'swiper/modules/navigation/navigation.scss'
+import 'swiper/modules/pagination/pagination.scss'
+
 document.addEventListener('DOMContentLoaded', () => {
     const portfolioButton = document.querySelector('.portfolio__button')
     const hiddenItems = document.querySelectorAll('.portfolio__content-item--hidden')
@@ -9,5 +15,20 @@ document.addEventListener('DOMContentLoaded', () => {
                 item.classList.add('visible')
             }
         })
+    })
+
+    new Swiper(".portfolio__swiper", {
+        slidesPerView: 1,
+        modules: [Autoplay, Navigation, Pagination],
+        speed: 500,
+        loop: true,
+        spaceBetween: 30,
+        navigation: {
+            nextEl: ".swiper-portfolio-button-next",
+            prevEl: ".swiper-portfolio-button-prev",
+        },
+        pagination: {
+            el: '.swiper-portfolio-pagination'
+        }
     })
 })
